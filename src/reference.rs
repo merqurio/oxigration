4754dfe470276be1d1c961e56de2cde13962f4af
Gabriel Maeztu <gabriel.maeztu@gmail.com>
2024-08-14 00:04:32 +0200

    Signed-off-by: Gabriel de Maeztu <gabriel.maeztu@gmail.com>

--- src/reference.rs
+++ src/reference.rs
@@ -1,14 +1,14 @@
+use crate::RelationalObject;
+use indexmap::IndexMap;
+use petgraph::algo::toposort;
+use petgraph::graphmap::DiGraphMap;
+use pg_query::NodeEnum;
 use std::collections::{HashMap, HashSet};
 use std::error::Error;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 use walkdir::WalkDir;
-use indexmap::IndexMap;
-use petgraph::graphmap::DiGraphMap;
-use petgraph::algo::toposort;
-use crate::RelationalObject;
-use pg_query::NodeEnum;
 
 /// Reads and processes a directory containing multiple subdirectories, each representing a type of
 /// database object.
@@ -75,31 +75,41 @@ use pg_query::NodeEnum;
 ///     }
 /// }
 /// ```
-pub fn read_desired_state(base_dir: &str) -> Result<IndexMap<String, RelationalObject>, Box<dyn Error>> {
+pub fn read_desired_state(
+    base_dir: &str,
+) -> Result<IndexMap<String, RelationalObject>, Box<dyn Error>> {
     let mut object_info: IndexMap<String, RelationalObject> = IndexMap::new();
 
     log::debug!("Reading desired state from {}", base_dir);
     for entry in WalkDir::new(base_dir).into_iter().filter_map(|e| e.ok()) {
-        if entry.file_type().is_file() && entry.path().extension().map_or(false, |ext| ext == "sql") {
+        if entry.file_type().is_file() && entry.path().extension().map_or(false, |ext| ext == "sql")
+        {
             let file_path = entry.path();
-            let schema_name = file_path.parent().and_then(|p| p.parent()).and_then(|p| p.file_name())
-                .and_then(|n| n.to_str()).ok_or("Invalid schema directory")?;
-            let object_type = file_path.parent().and_then(|p| p.file_name())
-                .and_then(|n| n.to_str()).ok_or("Invalid object type directory")?;
-            
+            let schema_name = file_path
+                .parent()
+                .and_then(|p| p.parent())
+                .and_then(|p| p.file_name())
+                .and_then(|n| n.to_str())
+                .ok_or("Invalid schema directory")?;
+            let object_type = file_path
+                .parent()
+                .and_then(|p| p.file_name())
+                .and_then(|n| n.to_str())
+                .ok_or("Invalid object type directory")?;
+
             let mut file = File::open(file_path)?;
             let mut contents = String::new();
             file.read_to_string(&mut contents)?;
 
-            if object_type.to_lowercase() == "table" {
-                let parsed_stmts = parse_change_stmts(&contents, "//// CHANGE", "GO", "name");
-                for (_, stmt) in parsed_stmts {
-                    if let Ok(relational_object) = process_sql_file(file_path, schema_name, object_type, &contents, Some(&stmt)) {
-                        object_info.insert(relational_object.object_name.clone(), relational_object);
-                    }
-                }
-            } else {
-                if let Ok(relational_object) = process_sql_file(file_path, schema_name, object_type, &contents, None) {
+            let parsed_stmts = parse_change_stmts(&contents, "//// CHANGE", "GO", "name");
+            for (_, stmt) in parsed_stmts {
+                if let Ok(relational_object) = build_relational_object(
+                    file_path,
+                    schema_name,
+                    object_type,
+                    &contents,
+                    Some(&stmt),
+                ) {
                     object_info.insert(relational_object.object_name.clone(), relational_object);
                 }
             }
@@ -109,11 +119,44 @@ pub fn read_desired_state(base_dir: &str) -> Result<IndexMap<String, RelationalO
     Ok(ordered_object_info)
 }
 
-fn process_sql_file(file_path: &Path, schema_name: &str, object_type: &str, contents: &str, stmt: Option<&Stmt>) -> Result<RelationalObject, Box<dyn Error>> {
+/// Builds a `RelationalObject` from the given parameters.
+///
+/// This function parses the SQL content and constructs a `RelationalObject`
+/// with the provided schema name, object type, and optional statement metadata.
+///
+/// # Arguments
+///
+/// * `file_path` - The path to the SQL file.
+/// * `schema_name` - The name of the schema.
+/// * `object_type` - The type of the database object (e.g., table, view).
+/// * `contents` - The SQL content of the file.
+/// * `stmt` - An optional statement with metadata.
+///
+/// # Returns
+///
+/// A `Result` containing the constructed `RelationalObject` or an error.
+///
+/// # Errors
+///
+/// Returns an error if the SQL content cannot be parsed or if required information is missing.
+fn build_relational_object(
+    file_path: &Path,
+    schema_name: &str,
+    object_type: &str,
+    contents: &str,
+    stmt: Option<&Stmt>,
+) -> Result<RelationalObject, Box<dyn Error>> {
     let parsed_content = pg_query::parse(&stmt.map_or(contents, |s| &s.value))?;
-    let first_object = parsed_content.protobuf.stmts.first().ok_or("No objects found in parsed content")?;
-    let node = first_object.stmt.as_ref().ok_or("No statement found in first object")?;
-    
+    let first_object = parsed_content
+        .protobuf
+        .stmts
+        .first()
+        .ok_or("No objects found in parsed content")?;
+    let node = first_object
+        .stmt
+        .as_ref()
+        .ok_or("No statement found in first object")?;
+
     if let Some(range_var) = match &node.node {
         Some(NodeEnum::RangeVar(range_var)) => Some(range_var),
         Some(NodeEnum::CreateStmt(create_stmt)) => create_stmt.relation.as_ref(),
@@ -121,21 +164,29 @@ fn process_sql_file(file_path: &Path, schema_name: &str, object_type: &str, cont
         Some(NodeEnum::DeleteStmt(delete_stmt)) => delete_stmt.relation.as_ref(),
         _ => None,
     } {
-        let object_name = file_path.file_stem()
+        let object_name = file_path
+            .file_stem()
             .and_then(|stem| stem.to_str())
             .map(|s| s.to_string())
             .unwrap_or_else(|| range_var.relname.clone());
         if object_name != range_var.relname {
-            log::warn!("Object name '{}' in file does not match name '{}' in SQL", object_name, range_var.relname);
+            log::warn!(
+                "Object name '{}' in file does not match name '{}' in SQL",
+                object_name,
+                range_var.relname
+            );
         }
         let schema_name = if range_var.schemaname.is_empty() {
             schema_name.to_string()
         } else {
             range_var.schemaname.clone()
         };
-        
+
         let key = match stmt {
-            Some(stmt) => format!("{}.{}.{}.{}", schema_name, object_type, object_name, stmt.change_name),
+            Some(stmt) => format!(
+                "{}.{}.{}.{}",
+                schema_name, object_type, object_name, stmt.change_name
+            ),
             None => format!("{}.{}.{}.{}", schema_name, object_type, object_name, "root"),
         };
 
@@ -155,11 +206,11 @@ fn process_sql_file(file_path: &Path, schema_name: &str, object_type: &str, cont
     }
 }
 
-#[derive(Debug)]
 /// Represents a statement with associated metadata.
 ///
 /// This struct encapsulates a named statement along with its value,
 /// dependencies, and additional properties.
+#[derive(Debug)]
 struct Stmt {
     /// The name of the statement.
     change_name: String,
@@ -168,7 +219,7 @@ struct Stmt {
     /// A set of dependencies for this statement.
     dependencies: HashSet<String>,
     /// Additional properties associated with the statement.
-    properties: HashMap<String, String>
+    properties: HashMap<String, String>,
 }
 
 /// Provides methods for creating and manipulating `Stmt` instances.
@@ -184,7 +235,7 @@ impl Stmt {
             change_name,
             value,
             dependencies,
-            properties
+            properties,
         }
     }
 }
@@ -200,28 +251,25 @@ impl Stmt {
 ///
 /// # Returns
 /// A list of object with the statement name, the text between the delimiters as value and the attributes defined in the start_delimiter as list of key values.
-/// [{name, value, attributes: {key: value}}]
-///
-/// # Examples
-/// ```
-/// let content = "//// CHANGE name=statement1 depends=statement2\nCREATE TABLE table1 (id INT);\nGO\n//// CHANGE name=statement2\nCREATE TABLE table2 (id INT);\nGO\n";
-/// let parsed_stmts = parse_change_stmts(content, "//// CHANGE", "GO", "name");
-/// assert_eq!(parsed_stmts.len(), 2);
-/// assert!(parsed_stmts.contains_key("statement1"));
-/// assert!(parsed_stmts.contains_key("statement2"));
-/// ```
-fn parse_change_stmts(content: &str, start_delimiter: &str, end_delimiter: &str, key: &str) -> IndexMap<String, Stmt> {
+fn parse_change_stmts(
+    content: &str,
+    start_delimiter: &str,
+    end_delimiter: &str,
+    key: &str,
+) -> IndexMap<String, Stmt> {
     let mut result: IndexMap<String, Stmt> = IndexMap::new();
     let mut dependencies: HashSet<String> = HashSet::new();
     let mut current_name = String::new();
     let mut value = String::new();
     let mut properties = HashMap::new();
     let mut in_statement = false;
+    let mut root_counter = 0;
 
     for line in content.lines() {
         if line.trim().starts_with(start_delimiter) {
             in_statement = true;
-            properties = line.trim_start_matches(start_delimiter)
+            properties = line
+                .trim_start_matches(start_delimiter)
                 .split_whitespace()
                 .filter_map(|attr| {
                     let mut parts = attr.split('=');
@@ -231,30 +279,79 @@ fn parse_change_stmts(content: &str, start_delimiter: &str, end_delimiter: &str,
             current_name = properties.get(key).cloned().unwrap_or_default();
         } else if line.trim() == end_delimiter {
             if in_statement {
-                result.insert(current_name.clone(), 
+                result.insert(
+                    current_name.clone(),
                     Stmt::new(
                         current_name.clone(),
                         value.trim().to_string(),
                         dependencies.clone(),
                         properties.clone(),
-                ));
+                    ),
+                );
                 dependencies.insert(current_name.clone());
                 current_name.clear();
                 value.clear();
                 properties.clear();
                 in_statement = false;
+            } else {
+                let root_name = format!("root{}", root_counter);
+                root_counter += 1;
+                result.insert(
+                    root_name.clone(),
+                    Stmt::new(
+                        root_name.clone(),
+                        value.trim().to_string(),
+                        dependencies.clone(),
+                        properties.clone(),
+                    ),
+                );
+                dependencies.insert(root_name.clone());
+                value.clear();
             }
         } else if in_statement {
+            value.push_str(line);
+            value.push('\n');
+        } else {
             value.push_str(line);
             value.push('\n');
         }
     }
 
+    if !value.trim().is_empty() {
+        let root_name = format!("root{}", root_counter);
+        result.insert(
+            root_name.clone(),
+            Stmt::new(
+                root_name,
+                value.trim().to_string(),
+                dependencies,
+                properties,
+            ),
+        );
+    }
+
     result
 }
 
+
+/// Determines the execution order of relational objects based on their dependencies.
+///
+/// This function takes an `IndexMap` of relational objects, where each object has a set of dependencies.
+/// It constructs a directed graph from these dependencies and performs a topological sort to determine
+/// the order in which the objects should be executed. If a cycle is detected in the dependencies, an error is returned.
+///
+/// TODO: This implementation does not handle references to objects inside the SQL content of the objects.
+///
+/// # Arguments
+/// * `object_info` - An `IndexMap` where the keys are object names and the values are `RelationalObject` instances.
+///
+/// # Returns
+/// A `Result` containing an `IndexMap` of the objects in the determined execution order, or an error if a cycle is detected.
+///
+/// # Errors
+/// Returns an error if a cycle is detected in the dependencies.
 fn determine_execution_order(
-    object_info: &IndexMap<String, RelationalObject>
+    object_info: &IndexMap<String, RelationalObject>,
 ) -> Result<IndexMap<String, RelationalObject>, Box<dyn std::error::Error>> {
     // Create a directed graph
     let mut graph = DiGraphMap::new();
@@ -268,8 +365,7 @@ fn determine_execution_order(
     }
 
     // Perform topological sort to determine execution order
-    let order = toposort(&graph, None)
-        .map_err(|_| "Cycle detected in dependencies")?;
+    let order = toposort(&graph, None).map_err(|_| "Cycle detected in dependencies")?;
 
     // Convert the order to a vector of strings
     let execution_order: Vec<String> = order.into_iter().map(|s| s.to_string()).collect();
@@ -281,4 +377,52 @@ fn determine_execution_order(
         }
     }
     Ok(ordered_object_info)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_change_stmts_with_delimiters() {
+        let content = "//// CHANGE name=statement1 depends=statement2\nCREATE TABLE table1 (id INT);\nGO\n//// CHANGE name=statement2\nCREATE TABLE table2 (id INT);\nGO\n";
+        let parsed_stmts = parse_change_stmts(content, "//// CHANGE", "GO", "name");
+        assert_eq!(parsed_stmts.len(), 2);
+        assert!(parsed_stmts.contains_key("statement1"));
+        assert!(parsed_stmts.contains_key("statement2"));
+    }
+
+    #[test]
+    fn test_parse_change_stmts_without_start_delimiter() {
+        let content = "CREATE TABLE table1 (id INT);\nGO\nCREATE TABLE table2 (id INT);\nGO\n";
+        let parsed_stmts = parse_change_stmts(content, "//// CHANGE", "GO", "name");
+        assert_eq!(parsed_stmts.len(), 2);
+        assert!(parsed_stmts.contains_key("root0"));
+        assert!(parsed_stmts.contains_key("root1"));
+    }
+
+    #[test]
+    fn test_parse_change_stmts_without_delimiters() {
+        let content = "CREATE TABLE table1 (id INT);";
+        let parsed_stmts = parse_change_stmts(content, "//// CHANGE", "GO", "name");
+        assert_eq!(parsed_stmts.len(), 1);
+        assert!(parsed_stmts.contains_key("root0"));
+    }
+
+    #[test]
+    fn test_parse_change_stmts_without_end_delimiters_and_one_statements() {
+        let content = "CREATE PROCEDURE sp1() LANGUAGE plpgsql AS $$ DECLARE val INTEGER; END $$; \n\nGO";
+        let parsed_stmts = parse_change_stmts(content, "//// CHANGE", "GO", "name");
+        assert_eq!(parsed_stmts.len(), 1);
+        assert!(parsed_stmts.contains_key("root0"));
+    }
+
+    #[test]
+    fn test_parse_change_stmts_without_end_delimiters_and_multiple_statements() {
+        let content = "CREATE PROCEDURE sp1() LANGUAGE plpgsql AS $$ DECLARE val INTEGER; END $$; \n\nGO\nCREATE PROCEDURE sp1() LANGUAGE plpgsql AS $$ DECLARE val INTEGER; END $$; \n\nGO";
+        let parsed_stmts = parse_change_stmts(content, "//// CHANGE", "GO", "name");
+        assert_eq!(parsed_stmts.len(), 2);
+        assert!(parsed_stmts.contains_key("root0"));
+        assert!(parsed_stmts.contains_key("root1"));
+    }
 }
