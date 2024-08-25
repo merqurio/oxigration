0efc400d8dbd1fddf96b1d37d79203813c684366
Gabriel Maeztu <gabriel.maeztu@gmail.com>
2024-08-25 11:29:47 +0200

    Add tests to the cli
    Signed-off-by: Gabriel de Maeztu <gabriel.maeztu@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,60 @@ use env_logger;
 use oxigration::{generate, init, migrate};
 use tokio;
 
+fn build_cli() -> Command {
+    Command::new("oxigration")
+        .about("Oxigration: DBMS Schema Migration Manager")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("init")
+                .about("Initialize the oxigration metadata to keep track of schema migrations")
+                .arg(
+                    Arg::new("connection")
+                        .short('c')
+                        .long("connection")
+                        .default_value("postgresql://postgres@0.0.0.0/postgres")
+                        .help("Database connection string"),
+                ),
+        )
+        .subcommand(
+            Command::new("generate")
+                .about("Generate source code of the schema from the DBMS for existing database")
+                .arg(
+                    Arg::new("dir")
+                        .short('d')
+                        .long("dir")
+                        .default_value("schemas/")
+                        .help("Directory to store generated schemas"),
+                )
+                .arg(
+                    Arg::new("connection")
+                        .short('c')
+                        .long("connection")
+                        .default_value("postgresql://postgres@0.0.0.0/postgres")
+                        .help("Database connection string"),
+                ),
+        )
+        .subcommand(
+            Command::new("migrate")
+                .about("Migrate the DBMS by applying changes to the database based on the source code schema files")
+                .arg(
+                    Arg::new("dir")
+                        .short('d')
+                        .long("dir")
+                        .default_value("schemas/")
+                        .help("Directory containing schema files"),
+                )
+                .arg(
+                    Arg::new("connection")
+                        .short('c')
+                        .long("connection")
+                        .default_value("postgresql://postgres@0.0.0.0/postgres")
+                        .help("Database connection string"),
+                ),
+        )
+}
+
 /// Oxigration: DBMS Schema Migration Manager
 ///
 /// Oxigration is a tool for managing database schema migrations, where a DBMS (Database Management System) organizes and maintains the structure of data, and a schema defines the blueprint of how data is stored and accessed. This tool simplifies the process of managing and applying database schema changes across different environments.
@@ -38,58 +92,7 @@ use tokio;
 #[tokio::main]
 async fn main() {
     env_logger::init();
-    let matches = Command::new("oxigration")
-        .about("Oxigration: DBMS Schema Migration Manager")
-        .subcommand_required(true)
-        .arg_required_else_help(true)
-        .subcommand(
-            Command::new("init")
-                .about("Initialize the database schema")
-                .arg(
-                    Arg::new("connection")
-                        .short('c')
-                        .long("connection")
-                        .default_value("postgresql://postgres@0.0.0.0.0/postgres")
-                        .help("Database connection string"),
-                ),
-        )
-        .subcommand(
-            Command::new("generate")
-                .about("Generate DBMS objects source code from the DBMS")
-                .arg(
-                    Arg::new("dir")
-                        .short('d')
-                        .long("dir")
-                        .default_value("schemas/")
-                        .help("Directory to store generated schemas"),
-                )
-                .arg(
-                    Arg::new("connection")
-                        .short('c')
-                        .long("connection")
-                        .default_value("postgresql://postgres@0.0.0.0.0/postgres")
-                        .help("Database connection string"),
-                ),
-        )
-        .subcommand(
-            Command::new("migrate")
-                .about("Migrate the DBMS by applying SQL changes accurately")
-                .arg(
-                    Arg::new("dir")
-                        .short('d')
-                        .long("dir")
-                        .default_value("schemas/")
-                        .help("Directory containing schema files"),
-                )
-                .arg(
-                    Arg::new("connection")
-                        .short('c')
-                        .long("connection")
-                        .default_value("postgresql://postgres@0.0.0.0.0/postgres")
-                        .help("Database connection string"),
-                ),
-        )
-        .get_matches();
+    let matches = build_cli().get_matches();
 
     match matches.subcommand() {
         Some(("init", sub_matches)) => {
@@ -138,47 +141,7 @@ mod tests {
 
     #[test]
     fn test_cli_init() {
-        let cmd = Command::new("oxigration")
-            .subcommand_required(true)
-            .arg_required_else_help(true)
-            .subcommand(
-                Command::new("init").arg(
-                    Arg::new("connection")
-                        .short('c')
-                        .long("connection")
-                        .default_value("postgresql://postgres@0.0.0.0.0/postgres"),
-                ),
-            )
-            .subcommand(
-                Command::new("generate")
-                    .arg(
-                        Arg::new("dir")
-                            .short('d')
-                            .long("dir")
-                            .default_value("schemas/"),
-                    )
-                    .arg(
-                        Arg::new("connection")
-                            .short('c')
-                            .long("connection")
-                            .default_value("postgresql://postgres@0.0.0.0.0/postgres"),
-                    ),
-            )
-            .subcommand(
-                Command::new("migrate")
-                    .arg(
-                        Arg::new("dir")
-                            .short('d')
-                            .long("dir")
-                            .default_value("schemas/"),
-                    )
-                    .arg(
-                        Arg::new("connection")
-                            .short('c')
-                            .long("connection")
-                            .default_value("postgresql://postgres@0.0.0.0.0/postgres"),
-                    ),
-            );
+        let cmd = build_cli();
 
         let matches = cmd.try_get_matches_from(vec![
             "oxigration",
@@ -191,6 +154,7 @@ mod tests {
         let matches = matches.unwrap();
         assert_eq!(matches.subcommand_name(), Some("init"));
         if let Some(sub_matches) = matches.subcommand_matches("init") {
+            let _c = sub_matches.get_one::<String>("connection").unwrap();
             assert_eq!(
                 sub_matches.get_one::<String>("connection").unwrap(),
                 "postgresql://test@localhost/test"
@@ -199,48 +163,25 @@ mod tests {
     }
 
     #[test]
-    fn test_cli_generate() {
-        let cmd = Command::new("oxigration")
-            .subcommand_required(true)
-            .arg_required_else_help(true)
-            .subcommand(
-                Command::new("init").arg(
-                    Arg::new("connection")
-                        .short('c')
-                        .long("connection")
-                        .default_value("postgresql://postgres@0.0.0.0.0/postgres"),
-                ),
-            )
-            .subcommand(
-                Command::new("generate")
-                    .arg(
-                        Arg::new("dir")
-                            .short('d')
-                            .long("dir")
-                            .default_value("schemas/"),
-                    )
-                    .arg(
-                        Arg::new("connection")
-                            .short('c')
-                            .long("connection")
-                            .default_value("postgresql://postgres@0.0.0.0.0/postgres"),
-                    ),
-            )
-            .subcommand(
-                Command::new("migrate")
-                    .arg(
-                        Arg::new("dir")
-                            .short('d')
-                            .long("dir")
-                            .default_value("schemas/"),
-                    )
-                    .arg(
-                        Arg::new("connection")
-                            .short('c')
-                            .long("connection")
-                            .default_value("postgresql://postgres@0.0.0.0.0/postgres"),
-                    ),
+    fn test_cli_init_default_connection() {
+        let cmd = build_cli();
+
+        let matches = cmd.try_get_matches_from(vec!["oxigration", "init"]);
+
+        assert!(matches.is_ok());
+        let matches = matches.unwrap();
+        assert_eq!(matches.subcommand_name(), Some("init"));
+        if let Some(sub_matches) = matches.subcommand_matches("init") {
+            assert_eq!(
+                sub_matches.get_one::<String>("connection").unwrap(),
+                "postgresql://postgres@0.0.0.0/postgres"
             );
+        }
+    }
+
+    #[test]
+    fn test_cli_generate() {
+        let cmd = build_cli();
 
         let matches = cmd.try_get_matches_from(vec![
             "oxigration",
@@ -268,47 +209,7 @@ mod tests {
 
     #[test]
     fn test_cli_migrate() {
-        let cmd = Command::new("oxigration")
-            .subcommand_required(true)
-            .arg_required_else_help(true)
-            .subcommand(
-                Command::new("init").arg(
-                    Arg::new("connection")
-                        .short('c')
-                        .long("connection")
-                        .default_value("postgresql://postgres@0.0.0.0.0/postgres"),
-                ),
-            )
-            .subcommand(
-                Command::new("generate")
-                    .arg(
-                        Arg::new("dir")
-                            .short('d')
-                            .long("dir")
-                            .default_value("schemas/"),
-                    )
-                    .arg(
-                        Arg::new("connection")
-                            .short('c')
-                            .long("connection")
-                            .default_value("postgresql://postgres@0.0.0.0.0/postgres"),
-                    ),
-            )
-            .subcommand(
-                Command::new("migrate")
-                    .arg(
-                        Arg::new("dir")
-                            .short('d')
-                            .long("dir")
-                            .default_value("schemas/"),
-                    )
-                    .arg(
-                        Arg::new("connection")
-                            .short('c')
-                            .long("connection")
-                            .default_value("postgresql://postgres@0.0.0.0.0/postgres"),
-                    ),
-            );
+        let cmd = build_cli();
 
         let matches = cmd.try_get_matches_from(vec![
             "oxigration",
